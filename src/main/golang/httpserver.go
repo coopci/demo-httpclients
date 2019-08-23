c17b53063777d59fe4e2bbdafc72b5f09c248a1c
Gu Bo <bgu@zuora.com>
2019-08-23 15:32:29 +0800

    Add /count

--- src/main/golang/httpserver.go
+++ src/main/golang/httpserver.go
@@ -3,32 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
- 	"sync"
+	"sync"
+	"sync/atomic"
 )
 
 func main() {
+	var ops int64
+	m := sync.Mutex{}
+	c := sync.NewCond(&m)
 
-m := sync.Mutex{}
-    c := sync.NewCond(&m)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+	http.HandleFunc("/wait", func(w http.ResponseWriter, r *http.Request) {
+		
+		c.L.Lock()
+		atomic.AddInt64(&ops, 1)
+		c.Wait()
+		atomic.AddInt64(&ops, -1)
+		c.L.Unlock()
+		fmt.Fprintf(w, "Go!")
+	})
 
-http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
-    })
-http.HandleFunc("/wait", func (w http.ResponseWriter, r *http.Request) {
-c.L.Lock()
-	c.Wait()
-c.L.Unlock()
-        fmt.Fprintf(w, "Go!")
-    })
+	http.HandleFunc("/release", func(w http.ResponseWriter, r *http.Request) {
+		c.Broadcast()
+		fmt.Fprintf(w, "Released")
+	})
 
-http.HandleFunc("/release", func (w http.ResponseWriter, r *http.Request) {
-        c.Broadcast()
-        fmt.Fprintf(w, "Released")
-    })
-
-    http.ListenAndServe(":8181", nil)
-
+	http.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
+		var c = atomic.LoadInt64(&ops);
+		output := fmt.Sprintf("%s%d", "Count:", c)
+                fmt.Fprintf(w, output)
+        })
+	http.ListenAndServe(":8181", nil)
 
 }
-
-
